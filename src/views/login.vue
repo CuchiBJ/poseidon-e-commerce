58e7fdf42f33258dddfd890dc28fe920585372de
Andres <andres_sandro@hotmail.com>
2021-02-20 18:57:30 -0300

    commit de login

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="login-dark fill-height">
+  <div class="login-dark fill-height"> 
     <v-container>
       <v-row align="center" justify="center">
         <v-col cols="12" sm="5" md="4">
