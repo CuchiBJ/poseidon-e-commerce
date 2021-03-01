fa2f8cb4dbf23489952e55102fa1c117c36f4b22
Fernando Cucchiarelli <cucchiarellifernando@gmail.com>
2021-03-01 19:38:41 -0300

    Productos

--- src/views/productos.vue
+++ src/views/productos.vue
@@ -4,7 +4,10 @@
       <v-col cols="3">
         <v-list-item-content>
           <v-row>
-            <v-col cols="12" class="d-flex justify-center">
+            <v-col
+              cols="12"
+              class="d-flex flex-column justify-center align-center"
+            >
               <v-text-field
                 @focus="searchClosed = false"
                 @blur="searchClosed = true"
@@ -16,26 +19,37 @@
                 placeholder="Search"
               >
               </v-text-field>
+              <v-btn elevation="2" large
+                >Filtrar<v-icon>mdi-filter</v-icon></v-btn
+              >
+              <v-checkbox
+                v-model="checkbox1"
+                :label="`Oferta`"
+              ></v-checkbox>
+              <v-checkbox
+                v-model="checkbox2"
+                :label="`Colores`"
+              ></v-checkbox>
             </v-col>
           </v-row>
         </v-list-item-content>
       </v-col>
-      <v-divider
-        inset
-        vertical>
-    </v-divider>
+      <v-divider inset vertical> </v-divider>
       <v-col cols="8">
         <v-row>
-          
           <v-col cols="12" class="d-flex justify-center">
             <h1 class="display-1 text-center mt-10">Las ofertas</h1>
-            </v-col>
+          </v-col>
         </v-row>
         <v-row>
-          
-          <v-col cols="12" class="">
+          <v-col cols="12">
             <v-card class="d-flex flex-row" color="gray mt-4">
-              <v-card :loading="loading" class="mx-auto my-12" width="200" height="200">
+              <v-card
+                :loading="loading"
+                class="mx-auto my-12"
+                width="200"
+                height="200"
+              >
                 <template slot="progress">
                   <v-progress-linear
                     color="deep-purple"
@@ -54,25 +68,20 @@
 
                 <v-card-text>
                   <v-row align="center" class="mx-0">
-                    <v-rating
-                      :value="4.5"
-                      color="amber"
-                      dense
-                      half-increments
-                      readonly
-                      size="10"
-                    ></v-rating>
-
                     <div class="grey--text ml-4">4.5 (413)</div>
                   </v-row>
 
                   <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
-
                 </v-card-text>
 
                 <v-divider class="mx-4"></v-divider>
               </v-card>
-              <v-card :loading="loading" class="mx-auto my-12" width="200" height="200">
+              <v-card
+                :loading="loading"
+                class="mx-auto my-12"
+                width="200"
+                height="200"
+              >
                 <template slot="progress">
                   <v-progress-linear
                     color="deep-purple"
@@ -91,20 +100,83 @@
 
                 <v-card-text>
                   <v-row align="center" class="mx-0">
-                    <v-rating
-                      :value="4.5"
-                      color="amber"
-                      dense
-                      half-increments
-                      readonly
-                      size="10"
-                    ></v-rating>
-
                     <div class="grey--text ml-4">4.5 (413)</div>
                   </v-row>
 
                   <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
+                </v-card-text>
 
+                <v-divider class="mx-4"></v-divider>
+              </v-card>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center">
+            <h1 class="display-1 text-center mt-10">Todo para el verano</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" class="">
+            <v-card class="d-flex flex-row" color="gray mt-4">
+              <v-card
+                :loading="loading"
+                class="mx-auto my-12"
+                width="200"
+                height="200"
+              >
+                <template slot="progress">
+                  <v-progress-linear
+                    color="deep-purple"
+                    height="10"
+                    indeterminate
+                  ></v-progress-linear>
+                </template>
+
+                <v-img
+                  height="100"
+                  width="200"
+                  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                ></v-img>
+
+                <v-card-title>Cafe Badilico</v-card-title>
+
+                <v-card-text>
+                  <v-row align="center" class="mx-0">
+                    <div class="grey--text ml-4">4.5 (413)</div>
+                  </v-row>
+                  <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
+                </v-card-text>
+
+                <v-divider class="mx-4"></v-divider>
+              </v-card>
+              <v-card
+                :loading="loading"
+                class="mx-auto my-12"
+                width="200"
+                height="200"
+              >
+                <template slot="progress">
+                  <v-progress-linear
+                    color="deep-purple"
+                    height="10"
+                    indeterminate
+                  ></v-progress-linear>
+                </template>
+
+                <v-img
+                  height="100"
+                  width="200"
+                  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                ></v-img>
+
+                <v-card-title>Cafe Badilico</v-card-title>
+
+                <v-card-text>
+                  <v-row align="center" class="mx-0">
+                    <div class="grey--text ml-4">4.5 (413)</div>
+                  </v-row>
+                  <div class="my-4 subtitle-1">$ • Italian, Cafe</div>
                 </v-card-text>
 
                 <v-divider class="mx-4"></v-divider>
@@ -124,6 +196,7 @@ export default {
     return {
       searchClosed: true,
       user: null,
+      productos: [],
     };
   },
   components: {},
@@ -132,11 +205,14 @@ export default {
       return this.user.rol == "admin";
     },
   },
+  created(){
+      this.productos=this.$store.getters.productos(this.$store)
+  }
 };
 </script>
 
 <style scoped>
-    .search {
-        max-width: 60%;
-    }
+.search {
+  max-width: 60%;
+}
 </style>
